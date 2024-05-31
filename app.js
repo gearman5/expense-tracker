ede0002c7846dd2bd2089a68403240b89ebb9e45
gilbert <gilbertmotlatso@gmail.com>
2024-05-31 21:03:38 +0200

    js still in progress

---
+++ app.js
@@ -0,0 +1,31 @@
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const addBtn = document.getElementById('add-btn');
+const expensesTableB = document.getElementById('expenses-table-b');
+const totalAmountCell = document.getElementById('total-amount')
+
+
+addBtn.addEventListener('click', function() {
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+
+    if (category == '') {
+        alert('Please select a category before adding amount');
+        return;
+    }
+
+    if (isNaN(amount) || amount <= 0) {
+        alert("Please enter amount in digits");
+        return;
+    }
+
+    if (date == '') {
+        alert('Please select a date');
+        return;
+    }
+});
